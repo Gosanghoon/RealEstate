2aa77e2694b892873d4a282b92ecf1dd12dadfa7
Gosanghoon <ahsxj66@naver.com>
2022-12-13 17:07:02 +0900

    20221213 - 두개 시트 같이 첫번째시트(전세), 두번째시트(매매) 가공 가능하게 수정

--- src/main/java/org/example/JMMultipleratioWrite.java
+++ src/main/java/org/example/JMMultipleratioWrite.java
@@ -11,37 +11,43 @@ import java.util.*;
 
 public class JMMultipleratioWrite {
 
-    public void writeExcel(HashMap<Integer, ArrayList<String>> hashMap, String filePath, String outFileNm){
+    public void writeExcel(HashMap<Integer, ArrayList<String>> hashMap, HashMap<Integer, ArrayList<String>> maemaeMap, String filePath, String outFileNm){
         //Empty Workbook create
         XSSFWorkbook workbook = new XSSFWorkbook();
 
-        //Empty Sheet create
-        XSSFSheet sheet = workbook.createSheet("RealEstate Data");
+        for(int i = 0 ; i < 2; i++){
 
-        // DATA에서 KETSET을 가져온다. 이 SET 값들을 조회하면서 데이터들을 SHEET에 입력한다.
-        Set<Integer> keyset = hashMap.keySet();
-        int rownum = 0;
+            //Empty Sheet create
+            String sheetName = i == 0 ? "전세" : "매매";
+            XSSFSheet sheet = workbook.createSheet(sheetName);
 
-        // 알아야할 점, TreeMap을 통해 생성된 ketSet은 for 조회시, 키값이 오름차순으로 조회된다.
-        for(Integer key : keyset){
-            Row row = sheet.createRow(rownum++);
-            ArrayList<String> objArr = hashMap.get(key);
-            int cellnum = 0;
-            for(Object obj : objArr){
-                Cell cell = row.createCell(cellnum++);
-                if (obj instanceof String){
-                    cell.setCellValue((String)obj);
-                } else if (obj instanceof Integer){
-                    cell.setCellValue((Integer)obj);
+            // DATA에서 KETSET을 가져온다. 이 SET 값들을 조회하면서 데이터들을 SHEET에 입력한다.
+            Set<Integer> keyset = i == 0 ? hashMap.keySet() : maemaeMap.keySet();
+            //Set<Integer> keyset = hashMap.keySet();
+            int rownum = 0;
+
+            for(Integer key : keyset){
+                Row row = sheet.createRow(rownum++);
+                ArrayList<String> objArr = i == 0 ? hashMap.get(key) : maemaeMap.get(key);
+                int cellnum = 0;
+                for(Object obj : objArr){
+                    Cell cell = row.createCell(cellnum++);
+
+                    if (obj instanceof String){
+                        cell.setCellValue((String)obj);
+                    } else if (obj instanceof Integer){
+                        cell.setCellValue((Integer)obj);
+                    }
                 }
+            }
+
+            try (FileOutputStream out = new FileOutputStream(new File(filePath, outFileNm))){
+                workbook.write(out);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
 
-        try (FileOutputStream out = new FileOutputStream(new File(filePath, outFileNm))){
-            workbook.write(out);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 
--- src/main/java/org/example/JMMultiplesratioRead.java
+++ src/main/java/org/example/JMMultiplesratioRead.java
@@ -7,30 +7,35 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class JMMultiplesratioRead {
 
     public static String filePath = "C:\\poi_temp";
-    public static String fileNm = "poi_reading_file_test.xlsx";
-    public static String outFileNm = "ksh_write_file_test.xlsx";
-    public static int restPrice = 0;
-    public static int hundredMilion = 0;
 
     public static void main(String[] args){
 
-        try (FileInputStream file = new FileInputStream(new File(filePath, fileNm))){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("읽을 파일명을 입력하세요.");
+        String inputFileNm = sc.next();
+        System.out.println("출력할 파일명을 입력하세요.");
+        String outputFileNm = sc.next();
+        System.out.println(inputFileNm + "   " + outputFileNm);
+
+        try (FileInputStream file = new FileInputStream(new File(filePath, inputFileNm))){
+
 
             //엑셀 파일로 Workbook instance를 생성한다.
             XSSFWorkbook workbook = new XSSFWorkbook(file);
 
-            //workbook의 첫번째 sheet를 가져온다.
-            XSSFSheet sheet = workbook.getSheetAt(0);
-
             HashMap<Integer, ArrayList<String>> resultMap = new HashMap<Integer, ArrayList<String>>();
+            HashMap<Integer, ArrayList<String>> maemaeMap = new HashMap<Integer, ArrayList<String>>();
 
+            for(int i = 0; i < workbook.getNumberOfSheets(); i++) {
+
+                //get sheet
+                XSSFSheet sheet = workbook.getSheetAt(i);
 
             /*
                 만약 특정 이름의 시트를 찾는다면 workbook.getSheet("찾는 시트의 이름");
@@ -41,74 +46,80 @@ public class JMMultiplesratioRead {
                 아니면 Iterator<Sheet> s = workbook.iterator()를 사용해서 조회해도 좋다.
              */
 
-            for (Row row : sheet){
-                //각각의 행에 존재하는 모든 cell을 순회한다.
-                Iterator<Cell> cellIterator = row.cellIterator();
+                for (Row row : sheet) {
+                    //각각의 행에 존재하는 모든 cell을 순회한다.
+                    Iterator<Cell> cellIterator = row.cellIterator();
 
-                while (cellIterator.hasNext()){
-                    Cell cell = cellIterator.next();
+                    while (cellIterator.hasNext()) {
+                        Cell cell = cellIterator.next();
 
-                    // cell의 타입을 확인하고 값을 가져온다.
-                    switch (cell.getCellType()){
+                        // cell의 타입을 확인하고 값을 가져온다.
+                        switch (cell.getCellType()) {
 
-                        case NUMERIC:
-                            //System.out.print("타니?");
-                            //getNumericCellValue 메서드는 기본으로 double형 반환
-                            System.out.print(cell.getColumnIndex() + "\t");
-                            System.out.print(cell.getNumericCellValue() + "\t");
+                            case NUMERIC:
+                                System.out.print(cell.getColumnIndex() + "\t");
+                                System.out.print(cell.getNumericCellValue() + "\t");
 
-                            if (cell.getRowIndex() > 0 && cell.getColumnIndex() == 0) {
-                                resultMap.put(cell.getRowIndex() -1, new ArrayList<String>());
-                                resultMap.get(cell.getRowIndex() -1).add(cell.getStringCellValue());
-                            }
-                            break;
+                                if (cell.getRowIndex() > 0 && cell.getColumnIndex() == 0) {
+                                    resultMap.put(cell.getRowIndex() - 1, new ArrayList<String>());
+                                    resultMap.get(cell.getRowIndex() - 1).add(cell.getStringCellValue());
+                                }
+                                break;
 
-                        case STRING:
+                            case STRING:
 
-                            String targetPrice = cell.getStringCellValue();
-                            String transePrice = "";
-                            //System.out.print(targetPrice + "\t");
-                            //System.out.print(cell.getRow());
+                                String targetPrice = cell.getStringCellValue();
+                                String transePrice = "";
+                                int restPrice = 0;
+                                int hundredMilion = 0;
 
-                            if (cell.getRowIndex() > 0 && cell.getColumnIndex() == 0) {
-                                resultMap.put(cell.getRowIndex() -1, new ArrayList<String>());
-                                resultMap.get(cell.getRowIndex() -1).add(cell.getStringCellValue());
+                                if (cell.getRowIndex() > 0 && cell.getColumnIndex() == 0) {
+                                    if(sheet.getSheetName().equals("전세")){
+                                        resultMap.put(cell.getRowIndex() - 1, new ArrayList<String>());
+                                        resultMap.get(cell.getRowIndex() - 1).add(cell.getStringCellValue());
+                                    } else if(sheet.getSheetName().equals("매매")){
+                                        maemaeMap.put(cell.getRowIndex() - 1, new ArrayList<String>());
+                                        maemaeMap.get(cell.getRowIndex() - 1).add(cell.getStringCellValue());
+                                    }
+                                }
 
-                            }
+                                if (cell.getRowIndex() > 0 && cell.getColumnIndex() == 1) {
 
-                            if (cell.getRowIndex() > 0 && cell.getColumnIndex() == 1){
-                                System.out.print(targetPrice + "\t");
-                                //1. 공백제거, 쉼표 제거
-                                if (targetPrice.contains(" "))
-                                    transePrice = targetPrice.replace(" ","");
+                                    //1. 공백제거, 쉼표 제거
+                                    if (targetPrice.contains(" "))
+                                        transePrice = targetPrice.replace(" ", "");
 
-                                if (targetPrice.contains(","))
-                                    transePrice = transePrice.replace(",","");
+                                    if (targetPrice.contains(","))
+                                        transePrice = transePrice.replace(",", "");
 
-                                //2. 천만원단위 가공
-                                if (!transePrice.substring(transePrice.indexOf("억")+1, transePrice.length()).equals(""))
-                                    restPrice = Integer.parseInt(transePrice.substring(transePrice.indexOf("억")+1, transePrice.length())) * 10000;
+                                    //2. 천만원단위 가공
+                                    if (!transePrice.substring(transePrice.indexOf("억") + 1, transePrice.length()).equals(""))
+                                        restPrice = Integer.parseInt(transePrice.substring(transePrice.indexOf("억") + 1, transePrice.length())) * 10000;
 
-                                //3. 억단위 가공
-                                hundredMilion = Integer.parseInt(targetPrice.substring(0, targetPrice.indexOf("억"))) * 100000000;
+                                    //3. 억단위 가공
+                                    hundredMilion = Integer.parseInt(targetPrice.substring(0, targetPrice.indexOf("억"))) * 100000000;
 
-                                //4. 합치기
-                                String finalPrice = String.valueOf(hundredMilion + restPrice);
-                                System.out.print(finalPrice + "\t");
-                                resultMap.get(cell.getRowIndex() -1).add(finalPrice);
-                            }
-                            break;
+                                    //4. 합치기
+                                    String finalPrice = String.valueOf(hundredMilion + restPrice);
+                                    //System.out.print(finalPrice + "\t");
+                                    if(sheet.getSheetName().equals("전세")){
+                                        resultMap.get(cell.getRowIndex() - 1).add(finalPrice);
+                                    } else if(sheet.getSheetName().equals("매매")){
+                                        maemaeMap.get(cell.getRowIndex() - 1).add(finalPrice);
+                                    }
+                                }
+                                break;
                         }
-                    //addArr.clear();
+                    }
                 }
             }
 
-            for(Integer i : resultMap.keySet()){
-                System.out.println("[key]:" + i + " [value]:" + resultMap.get(i));
+            for(Integer i : maemaeMap.keySet()){
+                System.out.println("[key]:" + i + " [value]:" + maemaeMap.get(i));
             }
 
             JMMultipleratioWrite jmMultipleratioWrite = new JMMultipleratioWrite();
-            jmMultipleratioWrite.writeExcel(resultMap, filePath, outFileNm);
+            jmMultipleratioWrite.writeExcel(resultMap,maemaeMap, filePath, outputFileNm);
             System.out.println();
 
         } catch (Exception e){
